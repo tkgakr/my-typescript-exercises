a3bb0ead3d13125537c67a4544f649fb3fc871e6
Akira Takagi <tkgakrwork@gmail.com>
2026-03-03 22:46:25 +0900

    wip: exercise-12

--- solutions/exercise-12/declarations/stats/index.d.ts
+++ solutions/exercise-12/declarations/stats/index.d.ts
@@ -1,3 +1,9 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export function getMaxIndex<T>(input: T[], comparator: function): number;
+    export function getMaxElement<T>(input: T[], comparator: function): T?;
+    export function getMinIndex<T>(input: T[], comparator: function): number;
+    export function getMinElement<T>(input: T[], comparator: function): T?;
+    export function getMedianIndex<T>(input: T[], comparator: function): number;
+    export function getMedianElement<T>(input: T[], comparator: function): T?;
+    export function getAverageValue<T>(input: T[], getValue: function): number?;
 }
